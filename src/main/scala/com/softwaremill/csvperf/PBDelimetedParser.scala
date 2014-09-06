71cd9f0acbdb75adf1280af8f161092fcc51b839
Maciej Bilas <maciej.bilas@gmail.com>
2014-09-06 17:43:16 +0200

    Fix parsing escaped fields.
    See http://maciejb.me/2014/07/11/a-csv-parser-moving-from-scala-parser-combinators-to-parboiled2/comment-page-1/#comment-165

--- src/main/scala/com/softwaremill/csvperf/PBDelimetedParser.scala
+++ src/main/scala/com/softwaremill/csvperf/PBDelimetedParser.scala
@@ -2,6 +2,8 @@ package com.softwaremill.csvperf
 
 import org.parboiled2._
 
+import scala.util.Try
+
 // https://github.com/sirthias/parboiled2/issues/61
 case class PBDelimetedParser(input: ParserInput, delimeter: String) extends Parser {
 
@@ -9,13 +11,14 @@ case class PBDelimetedParser(input: ParserInput, delimeter: String) extends Pars
 
   def DELIMITER_TOKEN = rule(capture(delimeter))
 
+  // combine 2 dquotes into 1
   def DQUOTE2 = rule("\"\"" ~ push("\""))
 
-  // combine 2 dquotes into 1
   def CRLF = rule(capture("\n\r" | "\n"))
+
   def NON_CAPTURING_CRLF = rule("\n\r" | "\n")
 
-  val delims = s"$delimeter\r\n"
+  val delims = s"$delimeter\r\n" + DQUOTE
 
   def TXT = rule(capture(!anyOf(delims) ~ ANY))
 
@@ -23,16 +26,17 @@ case class PBDelimetedParser(input: ParserInput, delimeter: String) extends Pars
 
   def SPACES: Rule0 = rule(oneOrMore(WHITESPACE))
 
-  def escaped = rule(optional(SPACES) ~
-    DQUOTE ~ (zeroOrMore(DELIMITER_TOKEN | TXT | CRLF | DQUOTE2) ~ DQUOTE ~
-    optional(SPACES)) ~> (_.mkString("")))
+  def escaped = rule(optional(SPACES) ~ DQUOTE ~ zeroOrMore(DELIMITER_TOKEN | TXT | CRLF | DQUOTE2)
+    ~ DQUOTE ~ optional(SPACES) ~> (_.mkString("")))
 
-  def nonEscaped = rule(zeroOrMore(TXT) ~> (_.mkString("")))
+  def nonEscaped = rule(zeroOrMore(TXT | capture(DQUOTE)) ~> (_.mkString("")))
 
   def field = rule(escaped | nonEscaped)
 
   def row: Rule1[Seq[String]] = rule(oneOrMore(field).separatedBy(delimeter))
 
   def file = rule(zeroOrMore(row).separatedBy(NON_CAPTURING_CRLF))
+
+  def parsed(): Try[Seq[Seq[String]]] = file.run()
 
 }
