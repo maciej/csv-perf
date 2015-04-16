4801b7f9a8ac920b58947038b22557eda479b4dc
Maciej Bilas <maciej.bilas@gmail.com>
2015-04-16 22:20:41 +0200

    PBDelimetedParser formatting.

--- src/main/scala/com/softwaremill/csvperf/PBDelimetedParser.scala
+++ src/main/scala/com/softwaremill/csvperf/PBDelimetedParser.scala
@@ -8,33 +8,24 @@ import scala.util.Try
 case class PBDelimetedParser(input: ParserInput, delimeter: String) extends Parser {
 
   def DQUOTE = '"'
-
   def DELIMITER_TOKEN = rule(capture(delimeter))
 
   // combine 2 dquotes into 1
   def DQUOTE2 = rule("\"\"" ~ push("\""))
-
   def CRLF = rule(capture("\n\r" | "\n"))
-
   def NON_CAPTURING_CRLF = rule("\n\r" | "\n")
 
   val delims = s"$delimeter\r\n" + DQUOTE
-
   def TXT = rule(capture(!anyOf(delims) ~ ANY))
-
   val WHITESPACE = CharPredicate(" \t")
-
   def SPACES: Rule0 = rule(oneOrMore(WHITESPACE))
 
   def escaped = rule(optional(SPACES) ~ DQUOTE ~ zeroOrMore(DELIMITER_TOKEN | TXT | CRLF | DQUOTE2)
     ~ DQUOTE ~ optional(SPACES) ~> (_.mkString("")))
-
   def nonEscaped = rule(zeroOrMore(TXT | capture(DQUOTE)) ~> (_.mkString("")))
 
   def field = rule(escaped | nonEscaped)
-
   def row: Rule1[Seq[String]] = rule(oneOrMore(field).separatedBy(delimeter))
-
   def file = rule(zeroOrMore(row).separatedBy(NON_CAPTURING_CRLF))
 
   def parsed(): Try[Seq[Seq[String]]] = file.run()
